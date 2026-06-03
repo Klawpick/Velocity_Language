56bd8169c895486add5fc217bfa91e2254f430a9
Klawpick <bn@klawpick.com>
2026-06-02 22:02:00 -0400

    Delete BLOCK_8_COMPILER.py
    didnt make it yet lol

--- BLOCK_8_COMPILER.py
+++
@@ -1,9 +0,0 @@
-# ==================================================
-# VELOCITY BLOCK 8 - COMPILER
-# ==================================================
-
-# Frontend
-# Intermediate Representation
-# Backend
-# Build system
-# Future native executable support
